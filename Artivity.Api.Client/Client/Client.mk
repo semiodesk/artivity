e9c9b8c921f0e62d0abaa8dc43c5fd43834c669f
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2015-08-08 10:50:27 +0200

    Bugfix: Strings without literal type URIs do render now. Bugfix: Formatting time values in ISO format now working under Windows.

--- Artivity.Api.Client/Client/Client.mk
+++ Artivity.Api.Client/Client/Client.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=Client
 ConfigurationName      :=Debug
-WorkspacePath          := "/home/sebastian/Projects/Artivity/Artivity.Api.Client"
-ProjectPath            := "/home/sebastian/Projects/Artivity/Artivity.Api.Client/Client"
+WorkspacePath          := "D:\Projects\Artivity\Artivity.Api.Client"
+ProjectPath            := "D:\Projects\Artivity\Artivity.Api.Client\Client"
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=Sebastian
-Date                   :=07/08/15
-CodeLitePath           :="/home/sebastian/.codelite"
-LinkerName             :=/usr/bin/g++
-SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
+Date                   :=08/08/2015
+CodeLitePath           :="C:\Program Files\CodeLite"
+LinkerName             :=C:/Build/Gcc/bin/g++.exe
+SharedObjectLinkerName :=C:/Build/Gcc/bin/g++.exe -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -34,7 +34,9 @@ ArchiveOutputSwitch    :=
 PreprocessOnlySwitch   :=-E
 ObjectsFileList        :="Client.txt"
 PCHCompileFlags        :=
-MakeDirCommand         :=mkdir -p
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=C:/Build/Gcc/bin/windres.exe
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -47,19 +49,19 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := /usr/bin/ar rcu
-CXX      := /usr/bin/g++
-CC       := /usr/bin/gcc
+AR       := C:/Build/Gcc/bin/ar.exe rcu
+CXX      := C:/Build/Gcc/bin/g++.exe
+CC       := C:/Build/Gcc/bin/gcc.exe
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := /usr/bin/as
+AS       := C:/Build/Gcc/bin/as.exe
 
 
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=/usr/share/codelite
+CodeLiteDir:=C:\Programme\CodeLite
 Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IntermediateDirectory)/Resource.cpp$(ObjectSuffix) $(IntermediateDirectory)/ActivityLog.cpp$(ObjectSuffix) $(IntermediateDirectory)/Activity.cpp$(ObjectSuffix) $(IntermediateDirectory)/Serializer.cpp$(ObjectSuffix) 
 
 
@@ -79,11 +81,11 @@ $(OutputFile): $(IntermediateDirectory)/.d $(Objects)
 	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
 
 MakeIntermediateDirs:
-	@test -d ./Debug || $(MakeDirCommand) ./Debug
+	@$(MakeDirCommand) "./Debug"
 
 
 $(IntermediateDirectory)/.d:
-	@test -d ./Debug || $(MakeDirCommand) ./Debug
+	@$(MakeDirCommand) "./Debug"
 
 PreBuild:
 
@@ -92,7 +94,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sebastian/Projects/Artivity/Artivity.Api.Client/Client/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/Projects/Artivity/Artivity.Api.Client/Client/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM "main.cpp"
 
@@ -100,7 +102,7 @@ $(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) "main.cpp"
 
 $(IntermediateDirectory)/Resource.cpp$(ObjectSuffix): Resource.cpp $(IntermediateDirectory)/Resource.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sebastian/Projects/Artivity/Artivity.Api.Client/Client/Resource.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Resource.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/Projects/Artivity/Artivity.Api.Client/Client/Resource.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Resource.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Resource.cpp$(DependSuffix): Resource.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Resource.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Resource.cpp$(DependSuffix) -MM "Resource.cpp"
 
@@ -108,7 +110,7 @@ $(IntermediateDirectory)/Resource.cpp$(PreprocessSuffix): Resource.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Resource.cpp$(PreprocessSuffix) "Resource.cpp"
 
 $(IntermediateDirectory)/ActivityLog.cpp$(ObjectSuffix): ActivityLog.cpp $(IntermediateDirectory)/ActivityLog.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sebastian/Projects/Artivity/Artivity.Api.Client/Client/ActivityLog.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/ActivityLog.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/Projects/Artivity/Artivity.Api.Client/Client/ActivityLog.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/ActivityLog.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/ActivityLog.cpp$(DependSuffix): ActivityLog.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/ActivityLog.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/ActivityLog.cpp$(DependSuffix) -MM "ActivityLog.cpp"
 
@@ -116,7 +118,7 @@ $(IntermediateDirectory)/ActivityLog.cpp$(PreprocessSuffix): ActivityLog.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/ActivityLog.cpp$(PreprocessSuffix) "ActivityLog.cpp"
 
 $(IntermediateDirectory)/Activity.cpp$(ObjectSuffix): Activity.cpp $(IntermediateDirectory)/Activity.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sebastian/Projects/Artivity/Artivity.Api.Client/Client/Activity.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Activity.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/Projects/Artivity/Artivity.Api.Client/Client/Activity.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Activity.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Activity.cpp$(DependSuffix): Activity.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Activity.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Activity.cpp$(DependSuffix) -MM "Activity.cpp"
 
@@ -124,7 +126,7 @@ $(IntermediateDirectory)/Activity.cpp$(PreprocessSuffix): Activity.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Activity.cpp$(PreprocessSuffix) "Activity.cpp"
 
 $(IntermediateDirectory)/Serializer.cpp$(ObjectSuffix): Serializer.cpp $(IntermediateDirectory)/Serializer.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sebastian/Projects/Artivity/Artivity.Api.Client/Client/Serializer.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Serializer.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/Projects/Artivity/Artivity.Api.Client/Client/Serializer.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Serializer.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Serializer.cpp$(DependSuffix): Serializer.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Serializer.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Serializer.cpp$(DependSuffix) -MM "Serializer.cpp"
 
