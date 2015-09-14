3518d4524164e7674dcee2c9f059390b6cc7e1ac
Sebastian Faubel <sebastian@semiodesk.com>
2015-09-14 11:28:46 +0200

    Minor improvements on Artivity Explorer.

--- Artivity.Explorer/Controls/Widgets/CompositionWidget.cs
+++ Artivity.Explorer/Controls/Widgets/CompositionWidget.cs
@@ -47,7 +47,7 @@ namespace ArtivityExplorer.Controls
 
         private void InitializeComponent()
         {
-			Color color = Color.FromName("Black");
+			Color color = Color.FromBytes(49, 55, 57);
 
 			ImageView icon = new ImageView(BitmapImage.FromResource("composition"));
 
