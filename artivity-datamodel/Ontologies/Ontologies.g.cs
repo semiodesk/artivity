86f02e8b38f55b045446d7a35f579b94be286d52
Sebastian Faubel <sebastian@semiodesk.com>
2015-10-26 16:44:24 +0100

    Removed Uri variables support from APID.

--- artivity-datamodel/Ontologies/Ontologies.g.cs
+++ artivity-datamodel/Ontologies/Ontologies.g.cs
@@ -1,5 +1,5 @@
 // Attention: This file is generated. Any modifications will eventually be overwritten.
-// Date: 10/26/2015 4:38:21 PM
+// Date: 10/26/2015 4:43:58 PM
 
 using System;
 using System.Collections.Generic;
