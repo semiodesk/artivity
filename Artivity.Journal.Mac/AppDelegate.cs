44b890d77238c23400d457c9fe0670fe75d0dca9
Sebastian Faubel <sebastian@semiodesk.com>
2016-04-22 15:09:12 +0200

    Implemented stand-alone Artivity Journal window on Mac OS X.

---
+++ Artivity.Journal.Mac/AppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.CoreGraphics;
+using MonoMac.ObjCRuntime;
+using MonoMac.AppKit;
+using MonoMac.WebKit;
+
+namespace Artivity.Journal.Mac
+{
+    public partial class AppDelegate : NSApplicationDelegate
+    {
+        MainWindowController mainWindowController;
+
+        public AppDelegate()
+        {
+        }
+
+        public override void FinishedLaunching(NSObject notification)
+        {
+            mainWindowController = new MainWindowController();
+            mainWindowController.Window.MakeKeyAndOrderFront(this);
+        }
+    }
+}
+
---
+++ Artivity.Journal.Mac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Artivity.Journal.Mac
+{
+    // Should subclass MonoMac.AppKit.NSResponder
+    [MonoMac.Foundation.Register("AppDelegate")]
+    public partial class AppDelegate
+    {
+    }
+}
+
