628bb4d885a0e2c3a773440e83bd9eb9427adbf7
Sebastian Faubel <sebastian@semiodesk.com>
2016-06-14 09:17:03 +0200

    Migrated Artivity.Apid.Mac to Xamarin Unified API project in order to enable Jenkins building it with xbuild/msbuild.

--- Artivity.DataModel/Ontologies/Ontologies.g.cs
+++ Artivity.DataModel/Ontologies/Ontologies.g.cs
@@ -1,5 +1,5 @@
 // Attention: This file is generated. Any modifications will eventually be overwritten.
-// Date: 6/13/2016 4:14:52 PM
+// Date: 6/14/2016 9:12:14 AM
 
 using System;
 using System.Collections.Generic;
