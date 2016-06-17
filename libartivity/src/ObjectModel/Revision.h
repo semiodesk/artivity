3c8cf8a9560be6e0f5407a2a7a466881dca9c43b
Moritz Eberl <moritz@semiodesk.com>
2016-06-17 12:24:02 +0200

    Added missing files

---
+++ libartivity/src/ObjectModel/Revision.h
@@ -0,0 +1,53 @@
+// LICENSE:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// AUTHORS:
+//
+//  Moritz Eberl <moritz@semiodesk.com>
+//  Sebastian Faubel <sebastian@semiodesk.com>
+//
+// Copyright (c) Semiodesk GmbH 2015
+
+#ifndef _ART_REVISION_H
+#define _ART_REVISION_H
+
+#include "EntityInfluence.h"
+
+namespace artivity
+{
+    class Revision;
+    typedef boost::shared_ptr<Revision> RevisionRef;
+
+    class Revision : public EntityInfluence
+    {        
+    public:
+        Revision() : EntityInfluence()
+        {
+            Resource::setType(prov::Revision);
+        }
+        
+        Revision(const char* uriref) : EntityInfluence(uriref)
+        {
+            Resource::setType(prov::Revision);
+        }
+    };
+}
+
+#endif // _ART_GENERATION_H
