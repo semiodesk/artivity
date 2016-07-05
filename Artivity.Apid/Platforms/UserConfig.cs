f25247c8d7929a2e825794b99cfdef92cd5e4f9e
Sebastian Faubel <sebastian@semiodesk.com>
2016-07-05 13:14:47 +0200

    Added missing files.

---
+++ Artivity.Apid/Platforms/UserConfig.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Artivity.Api.Platforms
+{
+    public class UserConfig
+    {
+        #region Members
+
+        [JsonIgnore]
+        public bool IsNew { get; set; }
+
+        public string Uid { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public string GetUserId()
+        {
+            if (Uid.StartsWith("urn:art:uid:"))
+            {
+                return Uid.Substring(Uid.LastIndexOf(':') + 1);
+            }
+            else
+            {
+                return Uid;
+            }
+        }
+
+        #endregion
+    }
+}
