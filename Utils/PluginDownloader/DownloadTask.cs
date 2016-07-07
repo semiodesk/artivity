c03a7fd5038e514a2f90e59a84124c971d6cded4
Moritz Eberl <moritz@semiodesk.com>
2016-07-07 15:04:58 +0200

    Added plugin downloader.

---
+++ Utils/PluginDownloader/DownloadTask.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PluginDownloader
+{
+    class DownloadTask : Task
+    {
+        #region Members
+        [Required]
+        public string Host { get; set; }
+
+        [Required]
+        public string TargetDirectory { get; set; }
+        #endregion
+
+        #region Methods
+
+        public override bool Execute()
+        {
+            Downloader.DownloadPlugins(new Uri(Host), new System.IO.DirectoryInfo(TargetDirectory));
+            return true;
+        }
+
+        #endregion
+    }
+}
