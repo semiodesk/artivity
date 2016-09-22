d09acbca1bd1d8e88e78aed6c823951865ad91fe
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2016-09-22 15:59:58 +0200

    Added experimental calendar to dashboard. Implemented refresh on window focus.

--- Artivity.Apid.Modules.Journal/app/js/app.js
+++ Artivity.Apid.Modules.Journal/app/js/app.js
@@ -51,57 +51,57 @@ explorerApp.factory('api', function ($http) {
 			return $http.get(endpoint + '/accounts/').then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getAccountConnectors: function () {
 			return $http.get(endpoint + '/accounts/connectors').then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getAccountConnector: function (connectorUri) {
 			return $http.get(endpoint + '/accounts/connectors?connectorUri=' + connectorUri).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getAccountConnectorStatus: function (sessionId) {
 			return $http.get(endpoint + '/accounts/connectors/status?sessionId=' + sessionId).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		authorizeAccount: function (parameter) {
 			var p = serialize(parameter);
-			
+
 			return $http.get(endpoint + '/accounts/authorize?' + p).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		installAccount: function (sessionId) {
 			return $http.get(endpoint + '/accounts/install?sessionId=' + sessionId).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		uninstallAccount: function (accountUri) {
 			return $http.get(endpoint + '/accounts/uninstall?accountUri=' + accountUri).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getAgents: function () {
 			return $http.get(endpoint + '/agents').then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getAgentAssociations: function () {
 			return $http.get(endpoint + '/agents/associations').then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		setAgents: function (agents) {
 			return $http.post(endpoint + '/agents', agents);
@@ -110,7 +110,7 @@ explorerApp.factory('api', function ($http) {
 			return $http.get(endpoint + '/agents?entityUri=' + entityUri).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getAgentIconUrl: function (associationUri) {
 			return endpoint + '/agents/software/icon?uri=' + associationUri;
@@ -156,7 +156,7 @@ explorerApp.factory('api', function ($http) {
 			return $http.get(endpoint + '/agents/user').then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		setUser: function (data) {
 			return $http.post(endpoint + '/agents/user', data);
@@ -171,56 +171,63 @@ explorerApp.factory('api', function ($http) {
 			return $http.get(endpoint + '/files?uri=' + entityUri).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getRecentFiles: function () {
 			return $http.get(endpoint + '/files/recent').then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getActivities: function (entityUri) {
-			return $http.get(endpoint + '/activities?uri=' + entityUri).then(
-				function (response) {
-					return response.data;
-				})
+			if (entityUri) {
+				return $http.get(endpoint + '/activities?uri=' + entityUri).then(
+					function (response) {
+						return response.data;
+					});
+			} else {
+				return $http.get(endpoint + '/activities').then(
+					function (response) {
+						return response.data;
+					});
+			}
 		},
 		getInfluences: function (entityUri) {
 			return $http.get(endpoint + '/influences?uri=' + entityUri).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getCanvases: function (entityUri, time) {
 			if (time !== undefined) {
 				return $http.get(endpoint + '/influences/canvas?uri=' + entityUri + '&timestamp=' + time).then(
 					function (response) {
 						return response.data;
-					})
+					});
 			} else {
 				return $http.get(endpoint + '/influences/canvas?uri=' + entityUri).then(
 					function (response) {
 						return response.data;
-					})
+					});
 			}
 		},
 		getLayers: function (entityUri) {
 			return $http.get(endpoint + '/influences/layers?uri=' + entityUri).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		getRenderings: function (entityUri, time) {
 			if (time !== undefined) {
 				return $http.get(endpoint + '/renderings?uri=' + entityUri + '&timestamp=' + time).then(
 					function (response) {
 						return response.data;
-					})
+					});
 			} else {
 				return $http.get(endpoint + '/renderings?uri=' + entityUri).then(
 					function (response) {
 						return response.data;
-					})
+					});
 			}
 		},
 		getRenderingUrl: function (entityUri) {
@@ -231,32 +238,32 @@ explorerApp.factory('api', function ($http) {
 				return $http.get(endpoint + '/stats/influences?uri=' + entityUri + '&timestamp=' + time).then(
 					function (response) {
 						return response.data;
-					})
+					});
 
 			} else {
 				return $http.get(endpoint + '/stats/influences?uri=' + entityUri).then(
 					function (response) {
 						return response.data;
-					})
+					});
 			}
 		},
 		getQueryResults: function (query) {
 			return $http.post(endpoint + '/query', query).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		exportFile: function (entityUri, fileName) {
 			return $http.get(endpoint + '/export?entityUri=' + entityUri + '&fileName=' + fileName).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		},
 		postComment: function (comment) {
 			return $http.post(endpoint + '/activities/comments', comment).then(
 				function (response) {
 					return response.data;
-				})
+				});
 		}
 	};
 });
