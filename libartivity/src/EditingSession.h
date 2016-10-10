5f23263acb72aa0fa23257d635dc5adac2b434a8
Moritz Eberl <moritz@semiodesk.com>
2016-10-10 15:17:41 +0200

    fixed a minor issue in libartivity.

--- libartivity/src/EditingSession.h
+++ libartivity/src/EditingSession.h
@@ -61,7 +61,7 @@ namespace artivity
 
         ImageRef document;
 
-        std::string EditingSession::createThumbnailFilePath();
+        std::string createThumbnailFilePath();
         std::string createImageFilePath(time_t time, int count);
         std::string getFileUri() { return _fileUri; }
 
