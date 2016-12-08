28aa158eab3f4db067ca2479e797e985965f10f9
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2016-12-08 17:17:24 +0100

    Implemented frameless window in Electron. Styled window control buttons to match Windows 10 default style.

--- Artivity.Apid.Modules.Journal/app/main.js
+++ Artivity.Apid.Modules.Journal/app/main.js
@@ -1,17 +1,19 @@
-const electron = require('electron')
+const electron = require('electron');
 
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
     // http://stackoverflow.com/questions/35876939/frameless-window-with-controls-in-electron-windows
-    var win = new BrowserWindow({width:1100, height: 600,  frame: true, icon: __dirname + '/img/icon.ico', show: false})
+    // NOTE: Setting the background color prevents flickering on resize:
+    // http://www.christianengvall.se/electron-white-screen-app-startup/
+    var win = new BrowserWindow({width:1100, height: 600,  frame: false, icon: __dirname + '/img/icon.ico', show: false, backgroundColor: '#1D1D1D'});
     win.setMenuBarVisibility(false);
-    win.loadURL(`file://${__dirname}/index.html`)
+    win.loadURL(`file://${__dirname}/index.html`);
     win.webContents.on('did-finish-load', function() {
         win.show();
     });
 })
 
- app.on('window-all-closed', function() {
+app.on('window-all-closed', function() {
   app.quit();
 });
