ed08e3ddcf61ebf75af84301440b0b7a1a2b186e
Sebastian Faubel <sebastian@semiodesk.com>
2016-12-20 15:30:27 +0100

    Implemented update notification dialog.

---
+++ Artivity.Apid.Modules.Journal/app/partials/dialogs/update-dialog.controller.js
@@ -0,0 +1,64 @@
+angular.module('explorerApp').controller('UpdateDialogController', UpdateDialogController);
+
+function UpdateDialogController(api, $scope, $filter, $uibModalInstance, $sce, updateService) {
+    var t = this;
+
+    t.title = $filter('translate')('UPDATE.TITLE');
+    t.update = {};
+    t.progress = null;
+    t.progressAnimate = false;
+    t.initialized = false;
+
+    updateService.isUpdateAvailable().then(init);
+
+    function init() {
+        var update = updateService.update;
+        var releaseNotesUrl = 'http://static.semiodesk.com/artivity/osx/1.6.101/changelog.html';
+
+        update.releaseNotesUrl = $sce.trustAsResourceUrl(releaseNotesUrl);
+
+        t.update = update;
+
+        updateService.on('progress', onDownloadProgress);
+        updateService.isUpdateDownloaded().then(function () {
+            // Set the download progress in case the update was downloaded prior to checking.
+            t.progress = {
+                percentComplete: 100
+            };
+
+            $scope.$apply();
+        });
+
+        $uibModalInstance.closed.then(function () {
+            updateService.off('progress', onDownloadProgress);
+        });
+
+        t.initialized = true;
+    }
+
+    function onDownloadProgress(progress) {
+        t.progress = progress;
+
+        $scope.$apply();
+    }
+
+    t.downloadUpdate = function () {
+        console.log('Downloading Update..');
+
+        t.progressAnimate = true;
+
+        updateService.downloadUpdate(t.update).then(function () {
+            console.log('Downloaded Update');
+        });
+    }
+
+    t.installUpdate = function () {
+        updateService.installUpdate(t.update).then(function () {
+            $uibModalInstance.dismiss('ok');
+        });
+    }
+
+    t.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    }
+};
