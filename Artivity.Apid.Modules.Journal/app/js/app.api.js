0985a93368d3485d87df4053d710f655222073d3
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2017-01-09 18:13:58 +0100

    Bugfix: Moved serialize function into app.api.js so that it is found in the minified release verison.

--- Artivity.Apid.Modules.Journal/app/js/app.api.js
+++ Artivity.Apid.Modules.Journal/app/js/app.api.js
@@ -278,3 +278,19 @@ app.factory('api', function ($http) {
 		}
 	};
 });
+
+function serialize(obj, prefix) {
+	var str = [];
+
+	for (var p in obj) {
+		if (obj.hasOwnProperty(p)) {
+			var k = prefix ? prefix + "[" + p + "]" : p,
+				v = obj[p];
+			str.push(typeof v == "object" ?
+				serialize(v, k) :
+				encodeURIComponent(k) + "=" + encodeURIComponent(v));
+		}
+	}
+
+	return str.join("&");
+}
