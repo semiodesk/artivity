5f6c16a1e30fedd7c3bdc702050160509e22eaf7
Moritz Eberl <moritz@semiodesk.com>
2017-01-16 17:45:02 +0100

    Added some mappings

---
+++ Artivity.DataModel/ObjectModel/Entities/InformationElement.cs
@@ -0,0 +1,18 @@
+﻿using Semiodesk.Trinity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Artivity.DataModel
+{
+    [RdfClass(NIE.InformationElement)]
+    public class InformationElement : Entity
+    {
+        public InformationElement(Uri uri) : base(uri) { }
+        public InformationElement(string uri) : base(uri) { }
+
+        [RdfProperty(NIE.isStoredAs)]
+        public FileDataObject DataObject { get; set; }
+    }
+}
