4d476a110c8abde9d3d3680650f575fad9761dcc
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2017-01-25 11:06:44 +0100

    Removed deprecated Artivity Journal projects.

--- Artivity.Journal.Mac/startAgent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-CONSOLE_USER=$(ps aux | grep console | grep -v grep | cut -d' ' -f1)
-userID=$(sudo -u $CONSOLE_USER id -u $CONSOLE_USER)
-
-#echo $CONSOLE_USER >> /tmp/script.out
-#echo $userID >> /tmp/script.out
-launchctl bootstrap gui/$userID /Library/LaunchAgents/com.semiodesk.artivity.plist
