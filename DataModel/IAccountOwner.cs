3b73d538bc358a7e31f08766a41bae4f35186dd2
Sebastian Faubel <sebastian@semiodesk.com>
2017-04-13 16:18:40 +0200

    Added missing files.

---
+++ DataModel/IAccountOwner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artivity.DataModel
+{
+    public interface IAccoutOwner : IPerson
+    {
+        #region Members
+
+        List<OnlineAccount> Accounts { get; }
+
+        #endregion
+    }
+}
