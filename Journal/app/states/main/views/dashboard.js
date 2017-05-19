be9ac7a6b10207b4d055ca885fd39d860f0eeea7
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2017-05-19 15:19:19 +0200

    Implemented delete project dialog.

--- Journal/app/states/main/views/dashboard.js
+++ Journal/app/states/main/views/dashboard.js
@@ -1,16 +1,53 @@
 (function () {
     angular.module('app').controller("DashboardViewController", DashboardViewController);
 
-    DashboardViewController.$inject = ['$rootScope', '$scope', '$state', '$stateParams', 'api', 'agentService', 'projectService'];
+    DashboardViewController.$inject = ['$rootScope', '$scope', '$state', '$stateParams', '$templateCache', '$mdMenu', '$mdDialog', 'api', 'agentService', 'projectService'];
 
-    function DashboardViewController($rootScope, $scope, $state, $stateParams, api, agentService, projectService) {
+    function DashboardViewController($rootScope, $scope, $state, $stateParams, $templateCache, $mdMenu, $mdDialog, api, agentService, projectService) {
         var t = this;
 
         t.openProject = function (project) {
             $rootScope.$broadcast('openProject', {
                 data: project
             });
-        }
+        };
+
+        t.openMenu = function (e, $mdMenu) {
+            $mdMenu.open(e);
+        };
+
+        t.deleteProject = function (e, project) {
+            var template = $templateCache.get('delete-project-dialog.html');
+
+            $mdDialog.show({
+                parent: angular.element(document.body),
+                template: template,
+                targetEvent: e,
+                controller: function($scope) {
+                    $scope.project = project;
+
+                    $scope.ok = function () {
+                        projectService.remove(project.Uri).then(function() {
+                            t.projects.splice(t.projects.indexOf(project), 1);
+                        });
+
+                        $mdDialog.hide();
+                    }
+
+                    $scope.cancel = $mdDialog.cancel;
+                }
+            });
+        };
+
+        t.loadUserAgent = function () {
+            return agentService.getCurrentUser().then(function (user) {
+                t.user = user;
+                t.userPhotoUrl = t.user ? t.user.PhotoUrl : '';
+            }, function () {
+                t.user = null;
+                t.userPhotoUrl = '';
+            });
+        };
 
         t.loadProjects = function () {
             return new Promise(function (resolve, reject) {
@@ -22,7 +59,7 @@
 
                 resolve(t.projects);
             });
-        }
+        };
 
         t.loadRecentFiles = function () {
             return new Promise(function (resolve, reject) {
@@ -34,18 +71,12 @@
 
                 resolve(t.files);
             });
-        }
+        };
 
         t.$onInit = function () {
-            agentService.getCurrentUser().then(function (user) {
-                    t.user = user;
-                    t.userPhotoUrl = t.user ? t.user.PhotoUrl : '';
-                }, function () {
-                    t.user = null;
-                    t.userPhotoUrl = '';
-                })
+            t.loadUserAgent()
                 .then(t.loadProjects)
                 .then(t.loadRecentFiles);
-        }
+        };
     }
 })();
