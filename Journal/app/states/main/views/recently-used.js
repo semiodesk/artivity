3af6bd3d885088ae53e5f004b96a10500b3067a0
Sebastian Faubel <sebastian@semiodesk.com>
2017-06-30 14:33:08 +0200

    Improved main sidebar. Improved project toolbar.

--- Journal/app/states/main/views/recently-used.js
+++ Journal/app/states/main/views/recently-used.js
@@ -1,13 +1,23 @@
 (function () {
     angular.module('app').controller("RecentlyUsedViewController", RecentlyUsedViewController);
 
-    RecentlyUsedViewController.$inject = ['$scope', '$state', '$stateParams', 'api'];
+    RecentlyUsedViewController.$inject = ['$rootScope', '$scope', '$state', '$stateParams', 'api'];
 
-    function RecentlyUsedViewController($scope, $state, $stateParams, api) {
+    function RecentlyUsedViewController($rootScope, $scope, $state, $stateParams, api) {
         var t = this;
+
+        t.findFiles = function (query) {
+            $rootScope.$broadcast('search', query);
+        }
 
         t.getRecentlyUsedFiles = function (callback) {
             return api.getRecentFiles().then(callback);
+        }
+
+        t.getFiles = function() {
+            return api.getRecentFiles().then(function(data) {
+                return data;
+            });
         }
     }
 })();
